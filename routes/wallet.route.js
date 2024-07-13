4e6ebddbb952e032acd2a9ff3882e1fd712156d0
senice222 <artemprimorksij7@gmail.com>
2024-07-13 11:53:26 +0300

    dockerf

--- routes/wallet.route.js
+++ routes/wallet.route.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const WalletService = require('../services/wallet.service');
-const { checkAuth } = require('../middleware/checkAuth');
+// const { checkAuth } = require('../middleware/checkAuth');
 const checkApiKey = require('../middleware/checkApiKey');
 
 const WalletRoute = Router();
 
-WalletRoute.post('/transfer', checkAuth, checkApiKey, WalletService.transfer);
-WalletRoute.post('/convert', checkAuth, checkApiKey, WalletService.convert);
+WalletRoute.post('/transfer', checkApiKey, WalletService.transfer);
+WalletRoute.post('/convert', checkApiKey, WalletService.convert);
 
 module.exports = WalletRoute;
